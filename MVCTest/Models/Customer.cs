8bf0593064aa5d9ea059a53cce26ddd13ea6fa94
Роман Валвенки <roomann@outlook.com>
2020-11-23 14:19:27 +0300

    final

--- MVCTest/Models/Customer.cs
+++ MVCTest/Models/Customer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace MVCTest.Models
 {
@@ -14,15 +15,14 @@ namespace MVCTest.Models
         [RegularExpression(@"^(\d{10}|\d{12})$", ErrorMessage ="Введите корректный ИНН")]
         public string INN { get; set; }
         [Required(ErrorMessage = "Введите  Имя")]
-
         public string name { get; set; }
         [Required]
         [RegularExpression(@"^(Индивидуальный предприниматель|Юридическое лицо)$")]
         public string Type { get; set; }
         public DateTime DateAdd { get; set; }
         public DateTime DateUpdate { get; set; }
-        [Required]
-        
+
+        [JsonIgnore]
         public ICollection<Founder> Founders { get; set; }
         public Customer() { Founders = new List<Founder>(); }
 
